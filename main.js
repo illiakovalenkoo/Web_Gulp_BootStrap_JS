a3b5b36bd57c7e1bdb5bedaf226a7b330099e95c
Illia Kovalenko <glekbiba2@gmail.com>
2025-09-22 23:58:05 +0200

    Update for video main.js

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 function slowScroll(id) { 
     $("html, body").animate({
         scrollTop: $(id).offset().top - 50   
-    }, 100);
+    }, 200);
     return false;
+}
+
+$('.video-play, #modal-video .close-button').on('click', function() { 
+    $("#modal-video").toggle();
+    $("body").toggleClass("overflow-hidden");
+    resizeVideo();
+});
+
+$(window).on('resize', function() { 
+    resizeVideo();
+}).resize();
+
+function resizeVideo() { 
+    $("iframe").each(function() { 
+        let width = $(this).width();
+        $(this).css("height", width / 1.77 + "px");
+    });
 }
