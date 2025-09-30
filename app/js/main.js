d84ccb2560e7969786e5becc28f1a3ecb3e60214
Illia Kovalenko <01184674@pw.edu.pl>
2025-09-30 19:18:36 +0200

    Change structure

---
+++ app/js/main.js
@@ -0,0 +1,36 @@
+function slowScroll(id) { 
+    $("html, body").animate({
+        scrollTop: $(id).offset().top - 50   
+    }, 200);
+    return false;
+}
+
+$("#subscribe").on("click", function() { 
+    let email = $("#email").val();
+    email = email.trim();
+    if(email.split("@").length != 2 || email.split(".").length != 2)  { 
+        $("#sub_form label").text("Вы ввели неверный email");
+        $("#sub_form label").fadeIn();
+    }
+
+    setTimeout(function() { 
+        $("#sub_form label").fadeOut()
+    }, 1500);
+});
+
+$('.video-play, #modal-video .close-button').on('click', function() { 
+    $("#modal-video").toggle();
+    $("body").toggleClass("overflow-hidden");
+    resizeVideo();
+});
+
+$(window).on('resize', function() { 
+    resizeVideo();
+}).resize();
+
+function resizeVideo() { 
+    $("iframe").each(function() { 
+        let width = $(this).width();
+        $(this).css("height", width / 1.77 + "px");
+    });
+}
